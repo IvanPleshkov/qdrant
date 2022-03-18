d1f367396ab51fa029fca9acf8cdd6cea041b3b9
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2022-03-18 13:10:42 +0400

    change main

--- src/main.rs
+++ src/main.rs
@@ -10,116 +10,96 @@ mod tonic;
 use consensus::Consensus;
 #[cfg(feature = "consensus")]
 use slog::Drain;
-use std::io::Error;
-use std::sync::Arc;
-use std::thread;
-use std::thread::JoinHandle;
-use storage::content_manager::toc::TableOfContent;
-
-use crate::common::helpers::create_search_runtime;
-use crate::settings::Settings;
-
 use tracy_client::*;
 
+/*
 #[global_allocator]
 static GLOBAL: ProfiledAllocator<std::alloc::System> =
     ProfiledAllocator::new(std::alloc::System, 100);
+*/
+
+use rand::{thread_rng, Rng};
+use segment::fixtures::index_fixtures::{
+    random_vector, FakeConditionChecker, TestRawScorerProducer,
+};
+use segment::index::hnsw_index::graph_layers::GraphLayers;
+use segment::index::hnsw_index::point_scorer::FilteredScorer;
+use segment::spaces::simple::CosineMetric;
+use segment::types::PointOffsetType;
+
+const NUM_VECTORS: usize = 5_000;
+const DIM: usize = 32;
+const M: usize = 16;
+const TOP: usize = 10;
+const EF_CONSTRUCT: usize = 64;
+const EF: usize = 64;
+const USE_HEURISTIC: bool = true;
+
+fn build_index(num_vectors: usize) -> (TestRawScorerProducer<CosineMetric>, GraphLayers) {
+    let mut rng = thread_rng();
+
+    let vector_holder = TestRawScorerProducer::new(DIM, num_vectors, CosineMetric {}, &mut rng);
+    let mut graph_layers = GraphLayers::new(num_vectors, M, M * 2, EF_CONSTRUCT, 10, USE_HEURISTIC);
+    let fake_condition_checker = FakeConditionChecker {};
+    for idx in 0..(num_vectors as PointOffsetType) {
+        let added_vector = vector_holder.vectors[idx as usize].to_vec();
+        let raw_scorer = vector_holder.get_raw_scorer(added_vector);
+        let scorer = FilteredScorer::new(&raw_scorer, &fake_condition_checker, None);
+        let level = graph_layers.get_random_layer(&mut rng);
+        graph_layers.link_new_point(idx, level, &scorer);
+    }
+    (vector_holder, graph_layers)
+}
 
 fn main() -> std::io::Result<()> {
-    let _span = Span::new("main", "main", file!(), line!(), 100);
-    let settings = Settings::new().expect("Can't read config.");
-    std::env::set_var("RUST_LOG", &settings.log_level);
-    env_logger::init();
+    let mut rng = thread_rng();
 
-    #[cfg(feature = "consensus")]
+    let (vector_holder, graph_layers) = build_index(NUM_VECTORS);
+
     {
-        // `raft` crate uses `slog` crate so it is needed to use `slog_stdlog::StdLog` to forward
-        // logs from it to `log` crate
-        let slog_logger = slog::Logger::root(slog_stdlog::StdLog.fuse(), slog::o!());
-
-        let (mut consensus, _message_sender) = Consensus::new(&slog_logger);
-        thread::Builder::new()
-            .name("raft".to_string())
-            .spawn(move || consensus.start())?;
+        let fake_condition_checker = FakeConditionChecker {};
+        let query = random_vector(&mut rng, DIM);
+        let raw_scorer = vector_holder.get_raw_scorer(query);
+        let scorer = FilteredScorer::new(&raw_scorer, &fake_condition_checker, None);
+        graph_layers.search(TOP, EF, &scorer);
     }
 
-    // Create and own search runtime out of the scope of async context to ensure correct
-    // destruction of it
-    let runtime = create_search_runtime(settings.storage.performance.max_search_threads)
-        .expect("Can't create runtime.");
+    for _ in 0..10 {
+        let fake_condition_checker = FakeConditionChecker {};
+        let query = random_vector(&mut rng, DIM);
+        let raw_scorer = vector_holder.get_raw_scorer(query);
+        let scorer = FilteredScorer::new(&raw_scorer, &fake_condition_checker, None);
+        graph_layers.search(TOP, EF, &scorer);
+    }
 
-    let runtime_handle = runtime.handle().clone();
+    let (vector_holder, graph_layers) = build_index(NUM_VECTORS * 10);
 
-    let toc = TableOfContent::new(&settings.storage, runtime);
-    runtime_handle.block_on(async {
-        for collection in toc.all_collections().await {
-            log::info!("Loaded collection: {}", collection);
-        }
-    });
-
-    let toc_arc = Arc::new(toc);
-
-    let mut handles: Vec<JoinHandle<Result<(), Error>>> = vec![];
-
-    #[cfg(feature = "web")]
     {
-        let toc_arc = toc_arc.clone();
-        let settings = settings.clone();
-        let handle = thread::Builder::new()
-            .name("web".to_string())
-            .spawn(move || actix::init(toc_arc, settings))
-            .unwrap();
-        handles.push(handle);
+        let fake_condition_checker = FakeConditionChecker {};
+        let query = random_vector(&mut rng, DIM);
+        let raw_scorer = vector_holder.get_raw_scorer(query);
+        let scorer = FilteredScorer::new(&raw_scorer, &fake_condition_checker, None);
+        graph_layers.search(TOP, EF, &scorer);
     }
 
-    if let Some(grpc_port) = settings.service.grpc_port {
-        let toc_arc = toc_arc.clone();
-        let settings = settings.clone();
-        let handle = thread::Builder::new()
-            .name("grpc".to_string())
-            .spawn(move || tonic::init(toc_arc, settings.service.host, grpc_port))
-            .unwrap();
-        handles.push(handle);
-    } else {
-        log::info!("gRPC endpoint disabled");
-    }
-
-    #[cfg(feature = "service_debug")]
     {
-        use parking_lot::deadlock;
-        use std::time::Duration;
+        let fake_condition_checker = FakeConditionChecker {};
+        let query = random_vector(&mut rng, DIM);
+        let raw_scorer = vector_holder.get_raw_scorer(query);
+        let scorer = FilteredScorer::new(&raw_scorer, &fake_condition_checker, None);
 
-        const DEADLOCK_CHECK_PERIOD: Duration = Duration::from_secs(10);
-
-        thread::Builder::new()
-            .name("deadlock_checker".to_string())
-            .spawn(move || loop {
-                thread::sleep(DEADLOCK_CHECK_PERIOD);
-                let deadlocks = deadlock::check_deadlock();
-                if deadlocks.is_empty() {
-                    continue;
-                }
-
-                let mut error = format!("{} deadlocks detected\n", deadlocks.len());
-                for (i, threads) in deadlocks.iter().enumerate() {
-                    error.push_str(&format!("Deadlock #{}\n", i));
-                    for t in threads {
-                        error.push_str(&format!(
-                            "Thread Id {:#?}\n{:#?}\n",
-                            t.thread_id(),
-                            t.backtrace()
-                        ));
-                    }
-                }
-                log::error!("{}", error);
-            })
-            .unwrap();
+        let mut points_to_score =
+            (0..1500).map(|_| rng.gen_range(0..(NUM_VECTORS * 10)) as u32);
+        scorer.score_iterable_points(&mut points_to_score, 1000, |_| {})
     }
 
-    for handle in handles.into_iter() {
-        handle.join().expect("Couldn't join on the thread")?;
+    for _ in 0..10 {
+        let fake_condition_checker = FakeConditionChecker {};
+        let query = random_vector(&mut rng, DIM);
+        let raw_scorer = vector_holder.get_raw_scorer(query);
+        let scorer = FilteredScorer::new(&raw_scorer, &fake_condition_checker, None);
+        graph_layers.search(TOP, EF, &scorer);
     }
-    drop(toc_arc);
-    drop(settings);
+
     Ok(())
 }
