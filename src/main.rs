59858ce54ef3dd8119124dccd820b4654af7750f
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2022-04-03 20:33:00 +0400

    fmt

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use segment::fixtures::index_fixtures::{
 };
 use segment::index::hnsw_index::graph_layers::GraphLayers;
 use segment::index::hnsw_index::point_scorer::FilteredScorer;
-use segment::spaces::simple::{ EuclidMetric, CosineMetric, DotProductMetric };
+use segment::spaces::simple::{CosineMetric, DotProductMetric, EuclidMetric};
 
 use bit_vec::BitVec;
 use segment::spaces::metric::Metric;
@@ -30,47 +30,46 @@ const EF_CONSTRUCT: usize = 64;
 const USE_HEURISTIC: bool = true;
 
 fn get_points() -> (Vec<Vec<VectorElementType>>, Vec<usize>) {
-    (vec![
-        vec![0.851758, 0.909671],
-        vec![0.823431, 0.372063],
-        vec![0.97826, 0.933157],
-        vec![0.39557, 0.306488],
-        vec![0.230606, 0.634397],
-        vec![0.514009, 0.399594],
-        vec![0.354438, 0.762611],
-        vec![0.0516154, 0.733427],
-        vec![0.769864, 0.288072],
-        vec![0.696896, 0.509403],
-        vec![0.805918, 0.923242],
-
-        
-        vec![0.36507, 0.513271],
-        vec![0.759294, 0.128909],
-        vec![0.547961, 0.877969],
-        vec![0.481519, 0.909654],
-        vec![0.905498, 0.0285553],
-        vec![0.452462, 0.346844],
-        vec![0.826136, 0.84315],
-        vec![0.350968, 0.92784],
-        vec![0.309734, 0.531955],
-        vec![0.2932, 0.186106],
-        vec![0.754536, 0.228132],
-        vec![0.151885, 0.107752],
-        vec![0.0787534, 0.433617],
-        vec![0.347133, 0.368639],  
-        
-    ],
-    vec![
-        0, 1, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 2,
-    ])
+    (
+        vec![
+            vec![0.851758, 0.909671],
+            vec![0.823431, 0.372063],
+            vec![0.97826, 0.933157],
+            vec![0.39557, 0.306488],
+            vec![0.230606, 0.634397],
+            vec![0.514009, 0.399594],
+            vec![0.354438, 0.762611],
+            vec![0.0516154, 0.733427],
+            vec![0.769864, 0.288072],
+            vec![0.696896, 0.509403],
+            vec![0.805918, 0.923242],
+            vec![0.36507, 0.513271],
+            vec![0.759294, 0.128909],
+            vec![0.547961, 0.877969],
+            vec![0.481519, 0.909654],
+            vec![0.905498, 0.0285553],
+            vec![0.452462, 0.346844],
+            vec![0.826136, 0.84315],
+            vec![0.350968, 0.92784],
+            vec![0.309734, 0.531955],
+            vec![0.2932, 0.186106],
+            vec![0.754536, 0.228132],
+            vec![0.151885, 0.107752],
+            vec![0.0787534, 0.433617],
+            vec![0.347133, 0.368639],
+        ],
+        vec![
+            0, 1, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 2,
+        ],
+    )
 }
 
 fn random_data() -> (Vec<Vec<VectorElementType>>, Vec<usize>) {
-    let count : usize = 5_000;
-    let dim : usize = 128;
+    let count: usize = 5_000;
+    let dim: usize = 128;
     let mut rng = thread_rng();
 
-    let mut points : Vec<Vec<VectorElementType>> = vec![];
+    let mut points: Vec<Vec<VectorElementType>> = vec![];
     for _ in 0..count {
         points.push(random_vector(&mut rng, dim));
     }
@@ -86,7 +85,8 @@ fn main() -> std::io::Result<()> {
     let (points, point_levels) = random_data();
     // let (points, point_levels) = get_points();
 
-    let mut graph_layers = GraphLayers::new(points.len(), M, M * 2, EF_CONSTRUCT, 10, USE_HEURISTIC);
+    let mut graph_layers =
+        GraphLayers::new(points.len(), M, M * 2, EF_CONSTRUCT, 10, USE_HEURISTIC);
     let fake_condition_checker = FakeConditionChecker {};
     let deleted = BitVec::from_elem(points.len(), false);
     let metric = DotProductMetric {};
@@ -99,7 +99,7 @@ fn main() -> std::io::Result<()> {
             deleted: &deleted,
         };
         let scorer = FilteredScorer::new(&raw_scorer, &fake_condition_checker, None);
-    
+
         let level = if idx < point_levels.len() {
             point_levels[idx]
         } else {
